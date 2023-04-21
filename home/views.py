25513f64c7cccc30e4a436ebc55d98dd3e2a3985
Arpit Sureka <surekaarpit@gmail.com>
2023-04-21 19:14:36 +0530

    updates

--- home/views.py
+++ home/views.py
@@ -37,7 +37,7 @@ def charts(request):
 	}
 	dens_mtech={
 		'2022':{"CSE":59,"BT":19,"EE":68,"ME":95,"CE":83,"Design":31,"CL":52,"DS":14},
-		'2023' : {"ME"	:3, "CST"	:3, "Development Studies"	:0, "School of Agro and Rural Technology"	:11, "BT"	:30, "CL"	:2, "Petroleum Science and Technology"	:29, "Chemistry"	:0, "Physics"	:0, "Robotics and Artificial Intelligence"	:16, "Data Science"	:13, "ECE"	:2, "CE"	:4, "CSE"	:59, "Design"	:4, "EP"	:0, "Materials Science and Technology"	:26, "Environment Engineering"	:17, "Geotechnical Engineering"	:17, "Energy"	:0, "Mathematics"	:2, "EEE"	:1, "MNC"	:2, "Manufacturing Science & Engineering"	:18, "Aerodynamics & Propulsion"	:8, "E-Mobility"	:1, "Computational Mechanics"	:8, "Fluids and Thermal Engineering"	:25, "Machine Design"	:24, "Infrastructure Engineering & Management"	:4, "Transportation Systems Engineering"	:17, "Communication Engineering"	:16, "VLSI"	:12, "Power Engineering"	:8, "Systems control & automation"	:3, "Structural Engineering"	:14, "SPML"	:16, "Biotechnology"	:0, "RF & Photonics"	:7, "Water Resources Engineering & Management"	:16, "Earth Systems Science and Engineering"	:8, "Disaster Mangement and Risk Reduction"	:0, "Food Science and Technology"	:5}
+		'2023':{"BT": 35, "CE": 94, "CL": 57, "CSE" : 60, "EE":68, "Interdisciplinary" : 20, "ME": 90}
 		}
 	dens_others={
 		'2022':{"Humanities":24,"Energy":11,"Physics":5,"Chemistry":19,"Mathematics":23},
@@ -176,7 +176,7 @@ def charts(request):
 			den = dens_mtech[years][bch]
 		elif(bch=="EE"):
 			num = len(Student.objects.filter(programs='M.Tech').filter(
-				branch__branchName__in=["ECE","EEE"]).filter(placed=True, year_placed=years))
+				branch__branchName__in=["ECE","EEE", "EE"]).filter(placed=True, year_placed=years))
 			den = dens_mtech[years][bch]
 		else:
 			num = len(mtech_all.filter(branch__branchName=bch))
@@ -260,7 +260,7 @@ def ajax_table_filter(request):
 			students = students.filter(branch=branch)
 		if program!='all':
 			students = students.filter(programs=program)
-		if year!='all' and year is not None:
+		if year:
 			students = students.filter(year_placed=year)
 		students = students.order_by(Lower(sortid))
 		branches = Branch.objects.all()
