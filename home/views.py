a263a2750707692f0f74f8e3c44ddb067ce911b3
Arpit Sureka <surekaarpit@gmail.com>
2023-04-22 00:05:21 +0530

    DFV

--- home/views.py
+++ home/views.py
@@ -33,7 +33,7 @@ def charts(request):
 	branches = ["CSE", "MNC", "EE", "ME", "CE", "CL", "EP", "CST", "BT", "DS","Physics", "Chemistry", "Mathematics", "Design", "Others"]
 	dens_btech={
 		'2022' : {"CSE":97,"MNC":62, "EE":130, "ME":88,"CL":60,"EP":34,"CST":38,"BT":33,"Design":38,"CE":73},
-		'2023' : {"CSE":108,"MNC":68 , "EE":154, "ME":101,"CL":69,"Physics":42,"CST":43,"BT":55,"Design":50,"CE":79}
+		'2023' : {"CSE":108,"MNC":68 , "EE":154, "ME":101,"CL":69,"EP":42,"CST":43,"BT":55,"Design":50,"CE":79}
 	}
 	dens_mtech={
 		'2022':{"CSE":59,"BT":19,"EE":68,"ME":95,"CE":83,"Design":31,"CL":52,"DS":14},
